8ef3832aa581e45e0229428e6624226c47cce07a
Nicolas Lepore <nlepore33@gmail.com>
2019-11-12 11:30:34 -0500

    us map

--- js/main.js
+++ js/main.js
@@ -10,18 +10,27 @@ var svg = d3.select("#map-US").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var projection = d3.geoMercator()
-    // .translate([(width / 2), (height / 2)])
-    // .scale(200);
+var projection = d3.geoAlbersUsa()
+    .translate([(width / 2), (height / 2)])
+    .scale(800);
 
 queue()
-    .defer(d3.json, "data/us-10m.v1.json")
-    // .defer(d3.csv, "data/global-malaria-2015.csv")
-    .await(function(error, mapTopUs) {
+    .defer(d3.json, "data/states-10m.json")
+    .defer(d3.csv, "data/cities-over-250k.csv")
+    .await(function(error, mapTopoUs, citiesTopo) {
 
-        console.log(mapTopUs.objects.states)
+        console.log(mapTopoUs)
+        console.log(citiesTopo)
 
-        var us = topojson.feature(mapTopUs, mapTopUs.objects.states).features
+        citiesTopo.forEach(function(d){
+            d.latitude = +d.latitude
+            d.longitude = +d.longitude
+            d.POP_2010 = +d.POP_2010
+        })
+
+        var us = topojson.feature(mapTopoUs, mapTopoUs.objects.states).features
+
+        console.log(us)
 
         svg.append("g")
             .selectAll("path")
@@ -31,7 +40,37 @@ queue()
             .attr("d", d3.geoPath()
                 .projection(projection)
             )
-            // .attr("fill", function (d) { return "black"});
+            .attr("fill", function (d) { return "white"})
+            .attr("stroke", "black");
+
+        // just added cities here in case we need them to pinpoint startups
+        // comment out if unneeded
+        svg.selectAll(".city")
+            .data(citiesTopo)
+            .enter().append("circle")
+            .attr("class", "city")
+            .attr("r", 5)
+            .attr("fill", "yellow")
+            .attr("transform", function(d) {
+                // console.log(d)
+                return "translate(" + projection([d.longitude, d.latitude]) + ")";
+            })
+
+        // also added city names
+        // comment out if unneeded
+        svg.append("g")
+            .selectAll(".city")
+            .data(citiesTopo)
+            .enter()
+            .append("text")
+            .attr("class", "city")
+            .attr("transform", function(d) {
+                // console.log(d)
+                return "translate(" + projection([d.longitude, d.latitude]) + ")";
+            })
+            .text(function(d){
+                return d.NAME
+            })
 
     })
 
